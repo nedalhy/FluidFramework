1ee8fa689f174eaacdfc72f5d4819c74af58a1be
nedalhy <nedalhy@gmail.com>
2022-09-13 18:22:40 +0200

    wip

--- packages/dds/tree/src/test/feature-libraries/editable-tree/editableTree.spec.ts
+++ packages/dds/tree/src/test/feature-libraries/editable-tree/editableTree.spec.ts
@@ -242,7 +242,7 @@ function expectTreeSequence(field: UnwrappedEditableField, expected: JsonableTre
     }
 }
 
-describe.only("editable-tree", () => {
+describe("editable-tree", () => {
     it("proxified forest", () => {
         const proxy = buildTestPerson();
         assert.ok(proxy);
